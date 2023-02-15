5722a71b9747c1b85add2558c97ccb0a078c4530
Yann Kamche <yannkamche@gmail.com>
2023-02-15 23:50:29 +0100

    Modified

--- Chapter1/Variables/declaringVariables.js
+++ Chapter1/Variables/declaringVariables.js
@@ -5,7 +5,7 @@ Declaring Variable: There are three ways of declaring variables, which are using
 - const
 */
 var myAge = "My age";
-var myName = "My name";// Declaring variables using va
+var myName = "My name";// Declaring variables using var
 
 console.log(myName);
 console.log(myAge);
@@ -14,4 +14,24 @@ let myHeight= "My height";
 let myConstant= "My constant";
 
 console.log(myConstant);
-console.log(myHeight);
+console.log(myHeight);// Declaring variables using let
+
+const someConstant = 3;
+//someConstant = 4;
+console.log(someConstant);
+
+/*WHen namimg a variable ensure the name is descriptive, 
+starts with lowercase and contain no spaces */
+
+//String: stores text value
+let singleString = 'Single String';// single string
+let doubleString = "Double String";// double string
+
+//backstrip
+let javaScript = "JavaScript"
+message = `Let's learn ${javaScript}`;
+
+console.log(singleString)
+console.log(doubleString);
+console.log(message);
+

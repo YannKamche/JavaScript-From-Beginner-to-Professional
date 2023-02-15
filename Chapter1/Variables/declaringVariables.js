10bccbd4dac3bf9f988e7aa49356d5a3f993421d
Yann Kamche <yannkamche@gmail.com>
2023-02-15 23:40:08 +0100

    Modified

--- Chapter1/Variables/declaringVariables.js
+++ Chapter1/Variables/declaringVariables.js
@@ -1,5 +1,17 @@
-let language = "JavaScript";
-let message = `let's learn ${language}`;
-var age = "This is my age";
-console.log(age);
-console.log(message);
+/*
+Declaring Variable: There are three ways of declaring variables, which are using:
+- let (block scope)
+- var (global scope)
+- const
+*/
+var myAge = "My age";
+var myName = "My name";// Declaring variables using va
+
+console.log(myName);
+console.log(myAge);
+
+let myHeight= "My height";
+let myConstant= "My constant";
+
+console.log(myConstant);
+console.log(myHeight);

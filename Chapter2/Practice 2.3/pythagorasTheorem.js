a87da1db134fc9ebdf76c62f132fe39c675d93f8
Yann Kamche <yannkamche@gmail.com>
2023-02-19 20:20:02 +0100

    Hypotenus of a triangle

---
+++ Chapter2/Practice 2.3/pythagorasTheorem.js
@@ -0,0 +1,7 @@
+/**
+ Write some code to calculate the hypotenuse of a triangle using the
+Pythagorean theorem when given the values of the other two sides.
+The theorem specifies that the relation between the sides of a rightangled triangle is a^2 + b^2 = c^2.
+ */
+
+let a = prompt("Enter the value of side a");

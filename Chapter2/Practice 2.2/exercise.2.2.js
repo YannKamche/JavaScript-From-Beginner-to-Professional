0c6fc15c492772430ad87464fb0a714e91857322
Yann Kamche <yannkamche@gmail.com>
2023-02-19 19:56:01 +0100

    Exercise 2.2

---
+++ Chapter2/Practice 2.2/exercise.2.2.js
@@ -0,0 +1,5 @@
+let name = "Yann";
+let age = "21";
+let codeJavaScript = "I can code JavaScript";
+
+console.log("Hello, my name is " + name + ". I am " + age + " years old and " + codeJavaScript);

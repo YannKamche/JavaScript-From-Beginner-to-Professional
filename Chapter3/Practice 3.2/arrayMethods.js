eee13eb77adcd48d359a3a8bb6090d2d5d6d7a74
Yann Kamche <yannkamche@gmail.com>
2023-07-21 23:42:49 +0100

    Added find, sort, reverse and index

--- Chapter3/Practice 3.2/arrayMethods.js
+++ Chapter3/Practice 3.2/arrayMethods.js
@@ -38,11 +38,45 @@ console.log(myFavoriteFruits);
 //splice(): Deletes an/some elements specified by index
 deletedArray = myConcatArray.splice(3, 3);
 console.log(myConcatArray);
-// 'delete' operator deletes a specifid
+// 'delete' operator deletes a specified
 delete deletedArray[0]; //Deletes first element of deletedArray
-console.log(deletedArray);
 
 /**
  Finding Elements
  */
+
 //find(): Checks a value in an array, returning undefined if not found
+//What is found in the find method is actually a function
+let findOrange = myFavoriteFruits.find(function(e) { return e === "orange"});
+console.log(findOrange);
+let findLemon = myFavoriteFruits.find(e => e === "lemon");
+console.log(findLemon);
+
+//indexOf(): returns the index on which the value is found.
+//If the value is not found, it will return -1
+let findIndexMango = myFavoriteFruits.indexOf("mango");
+console.log(findIndexMango); 
+
+//lastIndexOf(): Find the last occurence of an element in a list
+let lastOccurenceOrange = myFavoriteFruits.lastIndexOf("orange");
+console.log(lastOccurenceOrange); //logs '0' since orange exists only once
+
+/**
+ Sorting Elements
+ */
+//sort(): Sorts number from small to hight and strings A-Z
+let names = ["James", "Alicia", "Faitha", "Paul", "John"];
+let ages = [20, 10, 24 , 21, 17, 7];
+console.log(names.sort()); //Sorts alphabetically
+console.log(ages.sort());
+
+/**
+ Reversing Elements
+ */
+//reverse(): reverses any array irrespective of it sorted or not
+let listOfCountries = ["Cameroon", "Ghana", "Nigeria", "South Africa"];
+console.log(listOfCountries.find(e => e === "Cameroon"));
+console.log(listOfCountries.reverse());
+
+
+

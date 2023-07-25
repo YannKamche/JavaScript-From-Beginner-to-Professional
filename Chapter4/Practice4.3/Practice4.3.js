4e7b371ccc4ca00b7c33be3419dc0616469faa16
Yann Kamche <yannkamche@gmail.com>
2023-07-25 10:49:43 +0100

    Added Chapter 4

---
+++ Chapter4/Practice4.3/Practice4.3.js
@@ -0,0 +1,12 @@
+//Create a Boolearn value for an ID variable
+let ID = true;
+
+/*Using ternary operator, create
+a message variable that will
+check whether their ID is valid and 
+either allow a person into a 
+venue or not */
+let message = true;
+
+message === ID ? console.log("Granted")
+: console.log("Denied");
---
+++ Chapter4/Practice4.4/Practice4.4.js
@@ -0,0 +1,12 @@
+/*
+Magic 8-Ball random answer generator
+ */
+
+/*
+1. Start by seĴing a variable that gets a random value assigned to
+it. The value is assigned by generating a random number 0-5, for
+6 possible results. You can increase this number as you add
+more results.
+ */
+let random = Math.floor(Math.random() * 5);
+console.log(random);

0595c7955d2f6a1cfac35a5d59d71e4f4acae0ad
Yann Kamche <yannkamche@gmail.com>
2023-07-25 10:49:21 +0100

    completed chapter3

--- Chapter3/Practice 3.5/objects.js
+++ Chapter3/Practice 3.5/objects.js
@@ -67,3 +67,9 @@ let newCompany = {
 };
 
 console.log(newCompany.address[0].number, newCompany.address[1].zipcode);
+
+const myArr3 = [3,6,8,9,3,55,553,434];
+myArr3.sort();
+console.log(myArr3.sort());
+myArr3.length = 0;
+console.log(myArr3[0]);
